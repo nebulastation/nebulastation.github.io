4cba5a7e79282254991148c1d8159a87093464fc
nebulastation <63479310+nebulastation@users.noreply.github.com>
2020-04-11 14:47:18 -0400

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleproject() {
-    var x = document.getElementsById("cagepiece");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
