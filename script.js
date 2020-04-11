ce3ec90060f26aa994c89b3580955b90178a24d6
nebulastation <63479310+nebulastation@users.noreply.github.com>
2020-04-11 14:17:32 -0400

    toggle

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleproject() {
+    var x = document.getElementByClass("cagepiece");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
